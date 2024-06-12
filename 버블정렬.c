f0a38a403593ae8d7c99bc3f2d2afc88786bccf3
hr030106 <rlagpfla0613@naver.com>
2024-06-12 17:23:41 +0900

    Add files via upload

---
+++ 버블정렬.c
@@ -0,0 +1,29 @@
+# include <stdio.h>
+# define MAX_SIZE 5
+
+// 버블 정렬
+void bubble_sort(int list[], int n){
+  int i, j, temp;
+
+  for(i = n - 1; i > 0; i--){
+    for(j = 0; j < i; j++){
+      if(list[j] > list[j+1]){
+        temp = list[j];
+        list[j] = list[j+1];
+        list[j+1] = temp;
+      }
+    }
+  }
+}
+
+int main(){
+  int i;
+  int n = MAX_SIZE;
+  int list[MAX_SIZE] = {7, 4, 5, 1, 3};
+
+  bubble_sort(list, n);
+
+  for(i = 0; i < n; i++){
+    printf("%d ", list[i]);
+  }
+}
