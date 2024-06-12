bdbe4f59e455583d8b1923b6604d82a66161acd7
hr030106 <rlagpfla0613@naver.com>
2024-06-12 17:14:20 +0900

    Add files via upload

---
+++ 삽입정렬.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        /* arr[0..i-1]에서 arr[i]보다 큰 요소들을 모두 오른쪽으로 이동 */
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void printArray(int arr[], int n) {
+    int i;
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    insertionSort(arr, n);
+    printf("Sorted array: \n");
+    printArray(arr, n);
+
+    return 0;
+}
