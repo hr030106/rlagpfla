e903eb3f7b59b7de9ff37d12ed37b9c02dd26b25
hr030106 <rlagpfla0613@naver.com>
2024-06-19 11:49:11 +0900

    Add files via upload

---
+++ 행렬곱셈.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+#define MAX_SIZE 10 // 최대 행렬 크기 정의
+
+void matrix_multiply(int mat1[][MAX_SIZE], int mat2[][MAX_SIZE], int result[][MAX_SIZE], int size) {
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            result[i][j] = 0;
+            for (int k = 0; k < size; ++k) {
+                result[i][j] += mat1[i][k] * mat2[k][j];
+            }
+        }
+    }
+}
+
+int main() {
+    int size;
+
+    printf("행렬의 크기를 입력하세요 (최대 10): ");
+    scanf("%d", &size);
+
+    int mat1[MAX_SIZE][MAX_SIZE], mat2[MAX_SIZE][MAX_SIZE], result[MAX_SIZE][MAX_SIZE];
+
+    printf("첫 번째 행렬을 입력하세요:\n");
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            scanf("%d", &mat1[i][j]);
+        }
+    }
+
+    printf("두 번째 행렬을 입력하세요:\n");
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            scanf("%d", &mat2[i][j]);
+        }
+    }
+
+    // 행렬 곱셈 함수 호출
+    matrix_multiply(mat1, mat2, result, size);
+
+    // 결과 행렬 출력
+    printf("결과 행렬:\n");
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
